780e8dd6941505c96e3996947bbd85266a174e8b
bankovv <bankovkonstantin@yandex.ru>
2025-02-14 17:42:01 +0300

    feat(shared): implement add and remove words in card-words-list

--- src/app/shared/components/card-settings/card-words-list/card-words-list.component.ts
+++ src/app/shared/components/card-settings/card-words-list/card-words-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-card-words-list',
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrl: './card-words-list.component.css'
 })
 export class CardWordsListComponent {
+
+  @ViewChild('addWord')
+  public addWordInput!: ElementRef;
+
+  public words: string[] = [];
+
+  ngOnInit() {
+
+    this.words.push('word1');
+    this.words.push('word2');
+    this.words.push('word3');
+
+  }
+
+  public addWordButtonClicked() {
+    this.addWord(this.addWordInput.nativeElement.value);
+  }
+
+  public removeWordButtonClicked(word: string) {
+    this.words.splice(this.words.indexOf(word), 1);
+  }
+
+  public addWord(word: string) {
+    word = word.trim();
+    if (!word || word.length === 0 || this.words.includes(word))
+      return;
+    this.words.push(word);
+    this.addWordInput.nativeElement.value = '';
+  }
 
 }
