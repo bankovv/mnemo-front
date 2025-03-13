bca738b0eed035f44d90df0dd6a9254a2ad53fa9
bankovv <bankovkonstantin@yandex.ru>
2025-03-13 20:40:55 +0300

    feat(features/create-card): integrate CardSettingsComponent with deck and empty card

--- src/app/features/create-card/components/create-card/create-card.component.ts
+++ src/app/features/create-card/components/create-card/create-card.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { OpenedDeckService } from '../../../opened-deck/services/opened-deck.service';
+import { DeckModel } from '../../../../shared/models/decks/deck.model';
+import { CardModel } from '../../../../shared/models/decks/card.model';
 
 @Component({
   selector: 'app-create-card',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './create-card.component.css'
 })
 export class CreateCardComponent {
+
+  private deckService = inject(OpenedDeckService);
+
+  public deck!: DeckModel;
+  public card!: CardModel;
+
+  constructor() {
+
+    this.deckService.onDeckChange(deck => this.deck = deck);
+
+    this.card = {
+      cardId: -1,
+      wordsOriginal: [],
+      wordsTranslate: [],
+      audioUrls: undefined!,
+      videoUrls: undefined!,
+      createdAt: new Date()
+    }
+
+  }
 
 }
--- src/app/features/create-card/create-card.module.ts
+++ src/app/features/create-card/create-card.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateCardComponent } from './components/create-card/create-card.component';
-
-
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [
     CreateCardComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ]
 })
 export class CreateCardModule { }
