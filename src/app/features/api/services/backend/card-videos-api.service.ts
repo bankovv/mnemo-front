8f40c33d0ad957593e54181bd03f48dc050a4243
bankovv <bankovkonstantin@yandex.ru>
2025-03-20 21:37:01 +0300

    feat(features/api): add create card-video methods to api and facade services

--- src/app/features/api/services/backend/card-videos-api.service.ts
+++ src/app/features/api/services/backend/card-videos-api.service.ts
@@ -3,6 +3,8 @@ import { inject, Injectable } from '@angular/core';
 import { API_CONFIG } from '../../config/api-config';
 import { Observable } from 'rxjs';
 import { CardVideosResponse } from '../../dtos/cards/card-videos-response.dto';
+import { CardVideoCreateRequest } from '../../dtos/card-videos/card-video-create-request.dto';
+import { CardVideoCreateResponse } from '../../dtos/card-videos/card-video-create-response.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +16,10 @@ export class CardVideosApiService {
 
   public getCardVideos(cardId: number): Observable<CardVideosResponse> {
     return this.http.get<CardVideosResponse>(`${this.apiUrl}/${cardId}`);
+  }
+
+  public addVideo(request: CardVideoCreateRequest, cardId: number): Observable<CardVideoCreateResponse> {
+    return this.http.post<CardVideoCreateResponse>(`${this.apiUrl}/${cardId}`, request, { withCredentials: true });
   }
 
 }
--- src/app/features/api/services/facades/card-videos-facade.service.ts
+++ src/app/features/api/services/facades/card-videos-facade.service.ts
@@ -1,6 +1,8 @@
 import { inject, Injectable } from '@angular/core';
 import { CardVideosApiService } from '../backend/card-videos-api.service';
 import { map, Observable } from 'rxjs';
+import { CardVideoCreateResponse } from '../../dtos/card-videos/card-video-create-response.dto';
+import { CardVideoCreateRequest } from '../../dtos/card-videos/card-video-create-request.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +13,17 @@ export class CardVideosFacadeService {
 
   public getVideos(cardId: number): Observable<string[]> {
     return this.api.getCardVideos(cardId).pipe(map(resp => resp.videos));
+  }
+
+  public addVideo(cardId: number, deckPublicId: string, videoUrl: string): Observable<CardVideoCreateResponse> {
+
+    const request: CardVideoCreateRequest = {
+      deckPublicId: deckPublicId,
+      videoUrl: videoUrl
+    }
+
+    return this.api.addVideo(request, cardId);
+
   }
 
 }
