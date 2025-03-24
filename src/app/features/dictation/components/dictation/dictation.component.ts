29477c9ca05bb5af3db605a818c6a905e49a3031
bankovv <bankovkonstantin@yandex.ru>
2025-03-24 15:36:37 +0300

    feat(features/dictation): integrate dictation service in component

--- src/app/features/dictation/components/dictation/dictation.component.ts
+++ src/app/features/dictation/components/dictation/dictation.component.ts
@@ -1,6 +1,8 @@
-import { ChangeDetectorRef, Component, inject, Input, ViewChild } from '@angular/core';
+import { Component, ElementRef, inject, Input, signal, ViewChild } from '@angular/core';
 import { CardComponent } from '../../../../shared/components/card/card.component';
 import { OpenedDeckService } from '../../../opened-deck/services/opened-deck.service';
+import { DictationService } from '../../services/dictation.service';
+import { DeckModel } from '../../../../shared/models/decks/deck.model';
 
 @Component({
   selector: 'app-dictation',
@@ -10,20 +12,76 @@ import { OpenedDeckService } from '../../../opened-deck/services/opened-deck.ser
 export class DictationComponent {
 
   private deckService = inject(OpenedDeckService);
-  private changeDetector = inject(ChangeDetectorRef);
+  private dictationService = inject(DictationService);
   private isQuestionOriginal!: boolean;
+
+  private deckChangeListener: (deck: DeckModel) => void = this.deckChanged.bind(this);
+  private isOriginalSideDefaultListener: (isOrig: boolean) => void = this.isOriginalSideDefaultChanged.bind(this);
 
   @ViewChild('question')
   public questionCard!: CardComponent;
   @ViewChild('answer')
   public answerCard!: CardComponent;
+  @ViewChild('answerInput')
+  public answerInput!: ElementRef;
 
-  ngAfterViewChecked() {
+  public counter = signal('');
+
+  ngOnInit() {
+    this.deckService.onDeckChange(this.deckChangeListener);
+    this.deckService.onIsOriginalSideDefaultChange(this.isOriginalSideDefaultListener);
+  }
+
+  ngOnDestroy() {
+    this.deckService.offDeckChange(this.deckChangeListener);
+    this.deckService.offIsOriginalSideDefaultChange(this.isOriginalSideDefaultListener);
+  }
+
+  private deckChanged() {
+
     this.isQuestionOriginal = this.deckService.isOriginalSideDefault;
     this.questionCard.isOnOriginalSide.set(this.isQuestionOriginal);
     this.answerCard.isOnOriginalSide.set(!this.isQuestionOriginal);
-    this.questionCard.updateWords(this.deckService.currentCard);
-    this.changeDetector.detectChanges();
+
+    this.dictationService.isOnOriginalSide = this.deckService.isOriginalSideDefault;
+    this.dictationService.cards = this.deckService.cards;
+    this.updateWords();
+    this.updateCounter();
+
+  }
+
+  private isOriginalSideDefaultChanged(isOriginalSideDefault: boolean) {
+    this.dictationService.isOnOriginalSide = isOriginalSideDefault;
+    this.updateWords();
+  }
+
+  public prevQuestion() {
+    this.dictationService.prevQuestion();
+    this.updateWords();
+    this.updateCounter();
+  }
+
+  public nextQuestion() {
+    this.dictationService.nextQuestion();
+    this.updateWords();
+    this.updateCounter();
+  }
+
+  public addAnswer() {
+    const answer = this.answerInput.nativeElement.value.trim();
+    if (!answer || answer.length === 0) return;
+    this.dictationService.addAnswer(answer);
+    this.updateWords();
+    this.answerInput.nativeElement.value = '';
+  }
+
+  private updateCounter() {
+    this.counter.set(this.dictationService.counter);
+  }
+
+  private updateWords() {
+    this.questionCard.words = this.dictationService.currentQuestionWords;
+    this.answerCard.words = this.dictationService.currentAnswerWords;
   }
 
 }
