c9b124f196fee83f7e8cc96a1c1d67504ee142ed
bankovv <bankovkonstantin@yandex.ru>
2025-03-25 20:53:23 +0300

    feat(features/dictation): implement dictation results component

--- src/app/features/dictation/components/dictation-results/dictation-results.component.ts
+++ src/app/features/dictation/components/dictation-results/dictation-results.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { DictationService } from '../../services/dictation.service';
+import { DictationCard } from '../../models/dictation-card.model';
 
 @Component({
   selector: 'app-dictation-results',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrl: './dictation-results.component.css'
 })
 export class DictationResultsComponent {
+
+  private dictationService = inject(DictationService);
+
+  public restartDictation() {
+    this.dictationService.restartDictation();
+  }
+
+  public get results() {
+    return this.dictationService.dictationResults;
+  }
+
+  public isRightAnswerMatched(card: DictationCard, word: string): boolean {    
+    return card.enteredAnswerWords.includes(word.trim());
+  }
+
+  public isEnteredAnswerMatched(card: DictationCard, word: string) {
+    return card.rightAnswerWords.includes(word.trim());
+  }
 
 }
