93f41c402244af5751817b673f5129092185ab9e
bankovv <bankovkonstantin@yandex.ru>
2025-03-25 15:08:24 +0300

    feat(features/dictation): generate dictation-results component

---
+++ src/app/features/dictation/components/dictation-results/dictation-results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dictation-results',
+  templateUrl: './dictation-results.component.html',
+  styleUrl: './dictation-results.component.css'
+})
+export class DictationResultsComponent {
+
+}
--- src/app/features/dictation/dictation.module.ts
+++ src/app/features/dictation/dictation.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DictationComponent } from './components/dictation/dictation.component';
 import { SharedModule } from '../../shared/shared.module';
+import { DictationResultsComponent } from './components/dictation-results/dictation-results.component';
 
 
 
 @NgModule({
   declarations: [
-    DictationComponent
+    DictationComponent,
+    DictationResultsComponent
   ],
   imports: [
     CommonModule,
